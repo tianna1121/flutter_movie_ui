ecab713f16030048a668996852b5aa7a62c73a0b
yang zhang <tianna1121@gmail.com>
2019-09-17 19:19:39 +0800

    add the plus floating button and watchnow flatbutton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'customicon.dart';
+
 void main() => runApp(MaterialApp(
       home: Home(),
       debugShowCheckedModeBanner: false,
@@ -49,10 +51,103 @@ class HomeScreenTopPart extends StatelessWidget {
                     fit: BoxFit.cover,
                     width: double.infinity,
                   ),
+                  Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            const Color(0x00000000),
+                            const Color(0xD9333333),
+                          ],
+                          stops: [
+                            0.0,
+                            0.9
+                          ],
+                          begin: FractionalOffset(0.0, 0.0),
+                          end: FractionalOffset(0.0, 1.0)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 120.0, left: 95.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'WATCH BEFORE EVERONE',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0,
+                                fontFamily: 'SF-Pro-Display-Bold'),
+                          ),
+                          Text(
+                            'The Punisher: Season 2',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 45.0,
+                                fontFamily: "SF-Pro-Display-Bold"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
+          Positioned(
+            top: 370.0,
+            right: -20.0,
+            child: FractionalTranslation(
+              translation: Offset(0.0, -0.5),
+              child: Row(
+                children: <Widget>[
+                  FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.add,
+                      color: Color(0xFFE52020),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 12.0,
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: RaisedButton(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 15.0, horizontal: 80.0),
+                      onPressed: () {},
+                      color: Color(0xFFE52020),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Watch Now',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0,
+                                fontFamily: 'SF-Pro-Display-Bold'),
+                          ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          RotatedBox(
+                            quarterTurns: 2,
+                            child: Icon(
+                              CustomIcons.back_icon,
+                              size: 25.0,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
